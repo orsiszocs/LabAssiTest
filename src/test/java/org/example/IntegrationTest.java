435905a201fc30d34bf7b263c2f57b81126c1ddb
Szőcs Orsolya <vampirereflex@gmail.com>
2021-04-13 13:15:34 +0300

    integration test hw

--- src/test/java/org/example/IntegrationTest.java
+++ src/test/java/org/example/IntegrationTest.java
@@ -7,7 +7,6 @@ import org.example.Exceptions.ValidatorException;
 import org.example.Repository.MemoryRepository.NotaRepo;
 import org.example.Repository.MemoryRepository.StudentRepo;
 import org.example.Repository.MemoryRepository.TemaLabRepo;
-import org.example.Repository.XMLFileRepository.TemaLabXMLRepo;
 import org.example.Validator.NotaValidator;
 import org.example.Validator.StudentValidator;
 import org.example.Validator.TemaLabValidator;
@@ -60,6 +59,57 @@ public class IntegrationTest {
 
     @Test
     public void tc_AddAll() {
+        StudentValidator studentValidator = new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(studentValidator);
+        TemaLabValidator temaLabValidator = new TemaLabValidator();
+        TemaLabRepo temaLabRepo = new TemaLabRepo(temaLabValidator);
+        NotaValidator notaValidator = new NotaValidator();
+        NotaRepo notaRepo = new NotaRepo(notaValidator);
+        try {
+            studentRepo.save(new Student(7,"Valid Name","937","email@email.com","Valid Name"));
+            assertEquals(7, (int) studentRepo.findOne(7).getId());
+            temaLabRepo.save(new TemaLab(7, "description", 2, 10));
+            assertEquals(7, (int) temaLabRepo.findOne(7).getId());
+            notaRepo.save(new Nota(7, "7", 7, 7.5, LocalDateTime.now()));
+            assertEquals(7, (int) notaRepo.findOne(7).getId());
+        } catch (ValidatorException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test()
+    public void tc_Incremental1() {
+        StudentValidator studentValidator = new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(studentValidator);
+        try {
+            studentRepo.save(new Student(7,"Valid Name","937","email@email.com","Valid Name"));
+            assertEquals(7, (int) studentRepo.findOne(7).getId());
+        } catch (ValidatorException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test()
+    public void tc_Incremental2() {
+        StudentValidator studentValidator = new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(studentValidator);
+        TemaLabValidator temaLabValidator = new TemaLabValidator();
+        TemaLabRepo temaLabRepo = new TemaLabRepo(temaLabValidator);
+        try {
+            studentRepo.save(new Student(7,"Valid Name","937","email@email.com","Valid Name"));
+            assertEquals(7, (int) studentRepo.findOne(7).getId());
+            temaLabRepo.save(new TemaLab(7, "description", 2, 10));
+            assertEquals(7, (int) temaLabRepo.findOne(7).getId());
+        } catch (ValidatorException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void tc_Incremental3() {
         StudentValidator studentValidator = new StudentValidator();
         StudentRepo studentRepo = new StudentRepo(studentValidator);
         TemaLabValidator temaLabValidator = new TemaLabValidator();
